3857ec66115fff67b0ac4847ecd5eaf53d9782c0
Stive627 <fossistive627@gmail.com>
2025-08-26 15:48:36 +0530

    i am lacking inspiration for blinking effect

--- src/components/Navbar/BookConsultation.jsx
+++ src/components/Navbar/BookConsultation.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './navbar.css'
 
 function BookConsultation() {
   return (
-    <button style={{backgroundColor:'rgba(232, 93, 4, 0.97)'}} className=' text-white py-2 px-1 rounded-full cursor-pointer text-[12px] lg:text-sm w-40 lg:w-44'>BOOK CONSULTATION</button>
+    <div className='relative'>
+      <div className=' w-7 h-7 shakebutton bg-black rounded-full absolute -right-3 -top-3'></div>
+      <button style={{backgroundColor:'rgba(232, 93, 4, 0.97)'}} className=' boxshadow text-white py-2 px-1 rounded-full cursor-pointer text-[12px] lg:text-sm w-40 lg:w-44'>BOOK CONSULTATION</button>
+    </div>
   )
 }
 
